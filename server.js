239e7956ba1f41ba4ca5e036cb389673a29336cc
Morye <morye.kim@gmail.com>
2015-08-26 09:19:02 -0700

    Update server.js
    updated

--- server.js
+++ server.js
@@ -3,3 +3,4 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 
 connect().use(serveStatic(__dirname)).listen(8080);
+console.log("Thanks, Jokum. Server has been launched successfully~")
